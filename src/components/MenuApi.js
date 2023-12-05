9b36dca0875a58e0fa443e33f19ff5d2409dfc53
SaiVenkat168 <saivenkatsalpala@gmail.com>
2023-12-05 21:24:52 +0530

    FoodReciepe

--- src/components/MenuApi.js
+++ src/components/MenuApi.js
@@ -15,7 +15,7 @@ const Menu = [
       category: "evening",
       price: "20₹",
       description:
-        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, at consectetur totam voluptatibus quibusdam iusto. Accusamus quas, soluta ipsam autem eius necessitatibus fugiat in . ",
+        "Allu pakoda, also known as aloo pakora, is a popular Indian snack made from sliced potatoes coated in a seasoned chickpea flour (gram flour or besan) batter and deep-fried until crispy. It is a favorite tea-time or rainy-day snack in many parts of India",
     },
     {
       id: 3,
@@ -24,16 +24,16 @@ const Menu = [
       category: "breakfast",
       price: "10₹",
       description:
-        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, at consectetur totam voluptatibus quibusdam iusto. Accusamus quas, soluta ipsam autem eius necessitatibus fugiat in . ",
+        "maize, a cereal grain first domesticated by indigenous peoples in southern Mexico thousands of years ago.  ",
     },
     {
       id: 4,
       image: "../images/chola.jpg",
-      name: "chola",
+      name: "cholle",
       category: "lunch",
       price: "50₹",
       description:
-        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, at consectetur totam voluptatibus quibusdam iusto. Accusamus quas, soluta ipsam autem eius necessitatibus fugiat in . ",
+        "Chole Bhature is a popular North Indian dish that consists of spicy chickpeas (chole) and deep-fried bread (bhature). It is a hearty and flavorful dish, often enjoyed for breakfast, lunch, or dinner.",
     },
     {
       id: 5,
@@ -42,7 +42,7 @@ const Menu = [
       category: "evening",
       price: "80₹",
       description:
-        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, at consectetur totam voluptatibus quibusdam iusto. Accusamus quas, soluta ipsam autem eius necessitatibus fugiat in . ",
+        "Pizza is a popular and versatile dish that originated in Italy but has become a beloved international staple. It typically consists of a round, flat dough base topped with tomato sauce, cheese, and various toppings.",
     },
     {
       id: 6,
@@ -51,7 +51,7 @@ const Menu = [
       category: "dinner",
       price: "180₹",
       description:
-        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, at consectetur totam voluptatibus quibusdam iusto. Accusamus quas, soluta ipsam autem eius necessitatibus fugiat in . ",
+        "A non-vegetarian thali is a traditional Indian meal that includes a variety of dishes, showcasing the diverse flavors and textures of meat-based dishes. Thali refers to a platter that typically consists of small portions of different dishes served on a single plate. A non-vegetarian thali would include meat or seafood dishes along with various accompaniments",
     },
     {
       id: 7,
@@ -60,7 +60,7 @@ const Menu = [
       category: "dinner",
       price: "60₹",
       description:
-        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, at consectetur totam voluptatibus quibusdam iusto. Accusamus quas, soluta ipsam autem eius necessitatibus fugiat in . ",
+        "There are cultural and regional variations where certain sweet dishes are consumed as part of the main meal. In some cuisines, especially in South Asia and the Middle East, sweet and savory flavors are combined in a single meal.",
     },
     {
       id: 8,
@@ -69,7 +69,7 @@ const Menu = [
       category: "lunch",
       price: "60₹",
       description:
-        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, at consectetur totam voluptatibus quibusdam iusto. Accusamus quas, soluta ipsam autem eius necessitatibus fugiat in . ",
+        "Rajma Chawal is a popular and classic North Indian dish that consists of red kidney beans (rajma) cooked in a thick, flavorful gravy, served with steamed rice (chawal). It's a comfort food that is enjoyed by people of all ages and is a staple in many households. ",
     },
     {
       id: 9,
@@ -78,7 +78,7 @@ const Menu = [
       category: "evening",
       price: "10₹",
       description:
-        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, at consectetur totam voluptatibus quibusdam iusto. Accusamus quas, soluta ipsam autem eius necessitatibus fugiat in . ",
+        "Samosa is a popular and savory snack that originated in the Indian subcontinent. It has become a well-loved appetizer and street food in various parts of the world. Samosas are usually triangular pastries filled with a mixture of spiced potatoes, peas, and sometimes meat.",
     },
   ];
   
